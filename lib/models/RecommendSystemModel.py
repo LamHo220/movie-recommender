a6c3da74c7615d7985edc15a32d6182a106eb93c
Chun Ho Lam <hoh27622@gmail.com>
2022-11-01 15:43:34 +0800

    add svd++ prediction

--- lib/models/RecommendSystemModel.py
+++ lib/models/RecommendSystemModel.py
@@ -2,6 +2,7 @@ from abc import abstractmethod, ABC
 from typing import Any, Callable, List, Tuple
 import numpy as np
 from numpy.typing import NDArray
+import pandas as pd
 
 """ 
 The idea of the construction of abstract class is came from https://colab.research.google.com/drive/1I5CoVTCmwKUMfV6iiGG5Emuk2OtrcgRT?usp=sharing#scrollTo=Ft_CJd64z8Bu&uniqifier=1
@@ -9,43 +10,47 @@ The idea of the construction of abstract class is came from https://colab.resear
 
 
 class RecommendSystemModel(ABC):
-    def __init__(self) -> None:
-        self.data = None
-
-        self.valid = None
-        self.train = None
-        self.test = None
-
-        self.n_users = None
-        self.n_items = None
-
+    def __init__(
+        self,
+        mode: str = None,
+        features: int = None,
+        lr: float = None,
+        epochs: int = None,
+        weight_decay: float = None,
+        stopping: float = None,
+        momentum: float = None,
+    ) -> None:
         pass
-
-    # @abstractmethod
-    def split(self, ratio: float, tensor: bool = False):
-        pass
-
-    # @abstractmethod
-    def data_loader(self) -> None:
-        pass
-
-    # @abstractmethod
-    def learn_to_recommend(self, 
-        
-        features: int = 10,
-        lr: float = 0.0002,
-        epochs: int = 101,
-        weight_decay: float = 0.02,
-        stopping: float = 0.001,
-    ) -> Tuple[NDArray, NDArray, float, float]:
-        pass
-
-    # @abstractmethod
-    def prediction(self, u: int, i: int) -> float:
-        pass
-
-    # @abstractmethod
-    def loss(self, P: NDArray, Q: NDArray) -> float:
-        pass
-
     
+    # @abstractmethod
+    def split(
+        self, ratio_train_test: float, ratio_train_valid: float, tensor: bool = False
+    ) -> List[NDArray]:
+        pass
+
+    # @abstractmethod
+    def data_loader(
+        self,
+        path: str = None,
+        nrows: int = None,
+        skiprows=None,
+        data: pd.DataFrame = None,
+        n_users: int = None,
+        n_items=None,
+    ) -> None:
+        pass
+
+    # @abstractmethod
+    def training(self) -> Tuple[NDArray, NDArray, float, float]:
+        pass
+
+    # @abstractmethod
+    def prediction(self, P: NDArray, Q: NDArray, u: int, i: int) -> float:
+        pass
+
+    # @abstractmethod
+    def loss(self, groundTruthData, P: NDArray, Q: NDArray) -> float:
+        pass
+
+    def optimize(self, error: float, id_user: int, id_item: int, weight_decay):
+        pass
